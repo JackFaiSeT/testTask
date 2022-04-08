8639414db9a1f402621caacec7b32d0527f6c20e
JackFaiSeT <archakov.andrey@lab-company.ru>
2022-04-08 03:51:48 +0300

    feat: create default layout

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app">
+    <Header :navigationMenu="navigationMenu"/>
+    <main>
+      <nuxt />
+    </main>
+  </div>
+</template>
+
+<script lang='ts'>
+import { computed, ComputedRef, defineComponent, useStore } from '@nuxtjs/composition-api'
+import Header from '@/components/headerBlock.vue'
+export default defineComponent({
+  components: {Header},
+  setup() {
+    const { dispatch, getters } = useStore()
+    const navigationMenu: ComputedRef<Array<any>> = computed(() => getters['layout/navigationMenu'])
+    dispatch('layout/initNavigationMenu')
+    return {
+      navigationMenu
+    }
+  },
+})
+</script>
